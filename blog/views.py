600ecd34597666d8c4ec665afc81b20db14e475a
yaswanth14 <yaswanth14333@gmail.com>
2022-04-16 09:50:35 +0530

    added slug and models

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, HttpResponse
+from blog.models import Blog
 
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
 
 def blog(request):
-    return render(request, 'bloghome.html')
+    blogs = Blog.objects.all()
+    context = {'blogs': blogs}
+    return render(request, 'bloghome.html', context)
 
 def blogpost(request, slug):
     return HttpResponse(f"Your are viewing {slug}")
