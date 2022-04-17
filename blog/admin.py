5f2994bf9d08c3f5ce945bdc16e05a40fe657cca
yaswanth14 <yaswanth14333@gmail.com>
2022-04-17 08:36:27 +0530

    added tiny mce text editor

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from blog.models import Blog
 
+class BlogAdmin(admin.ModelAdmin):
+    class Media:
+        css = {
+            "all":("css/main.css",)
+        }
+
+
+        js = ("js/blog.js",)
+
 # Register your models here.
-admin.site.register(Blog)
+admin.site.register(Blog, BlogAdmin)
+
+
