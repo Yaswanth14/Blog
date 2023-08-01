473dea18a83c11f22c2da4194307e84ca601e3bb
Yaswanth14 <yaswanth14333@gmail.com>
2023-08-01 21:34:28 +0530

    Filter by search and Flash messages

--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,15 @@ import email
 from django.shortcuts import render, HttpResponse
 from blog.models import Blog, Contact
 from math import ceil
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
+    messages.success("Welcome to SnoozeWrites")
     return render(request, 'index.html')
 
 def blog(request):
-    no_of_posts = 7
+    no_of_posts = 3
     page = request.GET.get('page')
     if page is None:
         page = 1
@@ -45,14 +47,34 @@ def blogpost(request, slug):
     return render(request, 'blogpost.html', context)
 
 def search(request):
-    return render(request, 'search.html')
+    # allPosts = Blog.objects.all()
+    query = request.GET['query']
+    if(len(query)>78):
+        allPosts = Blog.objects.none()
+    else:
+        allPostsTitle = Blog.objects.filter(title__icontains=query)
+        allPostsContent = Blog.objects.filter(content__icontains=query)
+        allPosts = allPostsTitle.union(allPostsContent)
+
+    if allPosts.count() == 0:
+        messages.warning(request, "Please search with correct keyword")
+    
+    params = { 'allPosts': allPosts, 'query':query}
+    return render(request, 'search.html', params)
 
 def contact(request):
+    messages.success(request, "Welcome to contact")
     if request.method == "POST":
         name = request.POST.get("name")
         email = request.POST.get("email")
         desc = request.POST.get("desc")
-        instance = Contact(name=name, email=email, desc=desc)
-        instance.save()
+
+        if len(name)<2 or len(email)<3 or len(desc)<4:
+            messages.error(request, "Please fill the form correctly")
+        else:
+            instance = Contact(name=name, email=email, desc=desc)
+            instance.save()
+            messages.success(request, "Your message has been recieved :)")
+
 
     return render(request, 'contact.html')
